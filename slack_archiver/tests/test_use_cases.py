9a18650df89305f7351cbf10180869ee50678585
Marc Cataford <c.marcandre@gmail.com>
2019-07-07 14:37:57 -0400

    test: add read config coverage

--- slack_archiver/tests/test_use_cases.py
+++ slack_archiver/tests/test_use_cases.py
@@ -1,2 +1,21 @@
-def test_placeholder():
-    assert "tests TBD" == "tests TBD"
+from pytest_mock import mocker
+from pytest import raises
+
+import json
+
+from slack_archiver.use_cases import read_config_file
+from slack_archiver.exceptions import NoConfigFileException
+
+
+def test_read_config_file_raises_no_config_exception_if_config_missing(mocker):
+    mocker.patch("pathlib.Path.is_file").return_value = False
+    with raises(NoConfigFileException) as excinfo:
+        config = read_config_file()
+
+
+def test_read_config_file_returns_json_decoded_file_if_config_exists(mocker):
+    mock_config_str = '{ "mock": "json" }'
+    mocker.patch("pathlib.Path.is_file").return_value = True
+    mocker.patch("builtins.open", mocker.mock_open(read_data=mock_config_str))
+    mock_config = read_config_file()
+    assert mock_config == json.loads(mock_config_str)
